bed92ab076b7cd5a543485c12bc5498fb8e88e3b
unknown <neerajarao2898@gmail.com>
2023-09-18 08:46:39 +0530

    markAnswerAsAccepted MODIFIED , getQuestions,getAllAnswers added

--- src/Controllers/questionController.js
+++ src/Controllers/questionController.js
@@ -15,13 +15,13 @@ const createQuestion = async (req, res) => {
 
     await newQuestion.save();
 
-    res.status(201).json({status: "true", data : newQuestion ,message: 'Question posted successfully' });
+    res.status(201).json({status: true, data : newQuestion ,message: 'Question posted successfully' });
   } catch (error) {
-    res.status(500).json({ status: "false", error: error.message });
+    res.status(500).json({ status: false, error: error.message });
   }
 };
 
-// Edit a question (only the owner can edit)
+// Edit a question (only the Author can edit)
 const editQuestion = async (req, res) => {
   try {
     const { title, body, tags } = req.body;
@@ -34,18 +34,35 @@ const editQuestion = async (req, res) => {
     );
 
     if (!updatedQuestion) {
-      return res.status(404).json({status: "false", message: 'Question not found' });
+      return res.status(404).json({status: false, message: 'Question not found' });
     }
 
-    res.status(200).json({status: "true", message: 'Question updated successfully', data: updatedQuestion });
+    res.status(200).json({status: true, message: 'Question updated successfully', data: updatedQuestion });
   } catch (error) {
     console.error(error);
-    res.status(500).json({status: "false", error: error.message });
+    res.status(500).json({status: false, error: error.message });
   }
 };
 
 
-// Answer a question
+const getQuestions = async (req,res)=>{
+  try {
+    let questionId=req.query.questionId
+    let updatedQuestion
+    if(questionId){
+       updatedQuestion = await Question.findById({_id:questionId}).populate('AcceptedAnswerId')
+    }else{
+       updatedQuestion = await Question.find().populate('user')
+    }
+      res.status(201).send({status:true,data:updatedQuestion})
+
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ status: false, error: error.message});
+  }
+}
+
+
 const answerQuestion = async (req, res) => {
   try {
     const { body  } = req.body;
@@ -53,6 +70,8 @@ const answerQuestion = async (req, res) => {
 
     const question = await Question.findById(questionId);
     if(!question) return res.status(400).json({status:false, message: "No Such Question Found"})
+    
+    if(body== '')return res.status(400).json({status:false, message: "body is empty"})
 
     const newAnswer = new Answer({
       body,
@@ -62,19 +81,22 @@ const answerQuestion = async (req, res) => {
 
     await newAnswer.save();
 
-    res.status(201).json({status: "true",data : newAnswer, message: 'Answer posted successfully'  });
+    res.status(201).json({status: true,data : newAnswer, message: 'Answer posted successfully'  });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ status: "false", error: error.message});
+    res.status(500).json({ status: false, error: error.message});
   }
 };
 
-// Mark an answer as accepted (only the owner can mark)
+
+// Mark an answer as accepted (only the Author can mark)
+
 const markAnswerAsAccepted = async (req, res) => {
   try {
     const { questionId, answerId } = req.params;
+    const { AcceptedAnswerId } = req.body;
 
-    const question = await Question.findById(questionId);
+    let question = await Question.findById(questionId);
     if(!question) return res.status(400).json({status:false, message: "No Such Question Found"})
 
     if (question.user.toString() !== req.user.userId.toString()) {
@@ -83,20 +105,43 @@ const markAnswerAsAccepted = async (req, res) => {
     const answer = await Answer.findById(answerId);
     if(!answer) return res.status(400).json({status:false, message: "No Such Answer Found"})
 
-    question.AcceptedAnswerId = answerId;
+      
+     question.AcceptedAnswerId = AcceptedAnswerId;
 
-    await question.save();
+     await question.save();
 
-    res.status(200).json({ status: "true",data:question , message: 'Answer marked as accepted' });
+    res.status(200).json({ status: true,data:question , message: 'Answer marked as accepted' });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ status: "false", error: error.message });
+    res.status(500).json({ status: false, error: error.message });
+    }
   }
-};
+  
+
+const getAllAnswers = async (req, res) => {
+  try {
+    const { questionId } = req.params;
+
+    const questionData = await Question.findById(questionId);
+    if(!questionData) return res.status(400).json({status:false, message: "No Such Question Found"})
+
+  
+    const answer = await Answer.find({question:questionId});
+    
+    res.status(200).json({ status: true,data:answer });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ status: false, error: error.message });
+  }
+}
+  
+
 
 module.exports = {
   createQuestion,
   editQuestion,
   answerQuestion,
   markAnswerAsAccepted,
+  getQuestions,
+  getAllAnswers,
 };
