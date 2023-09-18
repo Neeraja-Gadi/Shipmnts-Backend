cb18f9245b3c310c7091d22cb0d515ca22a2e307
unknown <neerajarao2898@gmail.com>
2023-09-18 08:37:30 +0530

    new  routes updated

--- src/router/questionsRouter.js
+++ src/router/questionsRouter.js
@@ -1,22 +1,24 @@
 const express = require('express');
 
-
 const router = express.Router();
 
 const {authenticateUser,authorizeUser} = require('../Middlewares/auth');
 
 const questionController = require('../Controllers/questionController');
 
-// Create a new question
 router.post('/questions',authenticateUser, questionController.createQuestion);
 
 // Edit a question (only the author can edit)
 router.put('/questions/:questionId', authenticateUser,authorizeUser,questionController.editQuestion);
 
-// Answer a question
 router.post('/questions/:questionId/answers',authenticateUser, questionController.answerQuestion);
+
+router.get('/questions/:questionId/answers',authenticateUser, questionController.getAllAnswers);
+
 
 // Mark an answer as accepted (only the author can mark)
 router.put('/questions/:questionId/answers/:answerId/accept', authenticateUser,authorizeUser,questionController.markAnswerAsAccepted);
+
+router.get('/questions',questionController.getQuestions)
 
 module.exports = router;
